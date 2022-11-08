b30289477198bca7132e151a819e5a1864379bc8
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-08 03:02:01 -0800

    Updating the dom with data from weather api

--- app.js
+++ app.js
@@ -9,15 +9,25 @@ const image = document.querySelector('.weather__image');
 const temperature = document.querySelector('.weather__temperature>.value');
 const weatherAPIKey = '6f0ddb9b2341d8c05fa0f41837915cc1';
 const weatherBaseEndpoint = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${weatherAPIKey}`;
+// Function to get searched data back
 const getWeatherByCityName = async (city) => {
     let endpoint = `${weatherBaseEndpoint}&q=${city}`;
     let response = await fetch(endpoint);
     const weather = await response.json();
-    console.log(weather);
+    return weather;
+};
+const dayOfWeek = () => {
+    return new Date().toLocaleDateString('en-EN', {'weekday': 'long'});
+}
+// Function to update dom
+const updateCurrentWeather = (data) => {
+    city.textContent = `${data.name}, ${data.sys.country}`;
+    day.textContent = dayOfWeek();
 };
 // Eventlistener to search to city
 searchInput.addEventListener('keydown', async (e) => {
     if(e.keyCode === 13) {
         let weather = await getWeatherByCityName(searchInput.value);
+        updateCurrentWeather(weather);
     }
 });
